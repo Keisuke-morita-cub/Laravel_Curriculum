ae1c171848a39360caa6e0ae116231f587ae7dce
Keisuke-morita-cub <legend-jorge@i.softbank.jp>
2021-06-16 10:54:13 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
+ {
+    return view('index')->with(['posts' => $post->getByLimit()]);
+ }
+    public function show(Post $post)
     {
-        return view('index')->with(['posts'=>$post->get()]);
+         return view('show')->with(['post' => $post]);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function index(Post $post)
-    {
-    return $post->get();
-    }
+    public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+}
 }
