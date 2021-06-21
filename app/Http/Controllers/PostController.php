217a4d3587862c7b5ba328c691bdca1242f621cc
Keisuke-morita-cub <legend-jorge@i.softbank.jp>
2021-06-21 15:31:30 +0900

    ”[add]記事投稿”

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,11 +8,21 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
- {
+    {
     return view('index')->with(['posts' => $post->getByLimit()]);
- }
+    }
     public function show(Post $post)
     {
          return view('show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store(Request $request,Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
